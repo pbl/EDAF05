6001541b31e8c8cbcbbfd42fd053bb53a2eb1755
pbl <philip.burenstam.linder@gmail.com>
2014-04-02 15:21:35 +0200

    Almost there

---
+++ labb2/CharTest.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class CharTest{
+	public static void main(String[] args){
+		// String currentWord = args[0];
+		// char[] content = currentWord.toCharArray();
+		// Arrays.sort(content);
+		// currentWord = new String(content);
+		// System.out.println(currentWord);
+		// int i = 1;
+		// do{	
+		// 	boolean[] test = new boolean[2];
+		// 	System.out.println(test[0]);	
+		// 	i--;
+		// } while(i!=0);	
+		
+		BufferedReader br = null;
+		try{
+			br = new BufferedReader(new FileReader(args[0]));
+		} catch(FileNotFoundException e){
+
+		}
+		String line = null;
+		try{
+				line = br.readLine();	
+			} catch(IOException e){
+
+			}	
+		while(line!=null){
+			System.out.println(line);
+			try{
+				line = br.readLine();	
+			} catch(IOException e){
+
+		 	}
+			
+		// }
+
+		// HashMap<String, HashMap<Integer, Boolean>> test1 = new HashMap<String, HashMap<Integer, Boolean>>();
+		// HashMap<Integer, Boolean> test2 = new HashMap<Integer, Boolean>();
+		// test2.put(0, null);
+
+		// // test.put("hej", test2);
+		// System.out.println(test2.get(0));
+		
+		String str = "tjena";
+		char ch = str.charAt(0);
+		char ch2 = 't';
+		System.out.println(ch==ch2);
+
+	}
+}
