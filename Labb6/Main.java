07f812131f4b31d632a3159ac7632569d0e66f31
soderb <dat12eso@student.lu.se>
2014-05-14 13:09:38 +0200

    parser + test

---
+++ Labb6/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+
+	public static void main (String[] args){
+		
+		Parser p = new Parser();
+		HashMap<Integer, Edge> theMap = p.parseInput(args[0]);
+
+		for(Map.Entry hej : theMap.entrySet()){
+			System.out.println("bög-staden är" + hej);
+		}
+		HashMap<Integer, ArrayList<Integer>> test = p.getNeighbours();
+
+		for(Map.Entry snopp : theMap.entrySet()){
+			System.out.println("snopppp " + snopp);
+		}
+	}	
+
+}
